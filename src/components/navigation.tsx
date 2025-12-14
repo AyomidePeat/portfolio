d00f84d785d69f8e0ed1656f2b3684cec0d72c46
Omosuyi Olawole <omooladev@gmail.com>
2025-12-14 18:00:36 +0100

    refacotor the design of all the pages to look great on mobile screens

--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -1,25 +1,21 @@
-"use client";
+'use client';
 
-import Link from "next/link";
+import Link from 'next/link';
 
 export function Navigation() {
+  
   return (
     <>
-      <nav className="fixed top-0 left-10 right-10 z-50 bg-background/80 backdrop-blur-md animate-fade-in hidden md:block">
+
+      <nav className="fixed bg-background/80 top-0 left-10 right-10 z-50  backdrop-blur-md animate-fade-in hidden md:block">
         <div className="px-2 py-4 flex items-center justify-between">
-          <Link
-            href="#home"
-            className="font-sans text-xl font-bold text-primary tracking-tight animate-underline-hover hover:text-accent transition-colors"
-          >
+          <Link href="#home" className="font-sans text-xl font-bold text-primary tracking-tight animate-underline-hover hover:text-accent transition-colors">
             Oladipupo Peace
           </Link>
           <ul className="flex space-x-6 text-foreground">
-            {["home", "about", "experience", "projects", "contact"].map((section) => (
+            {['home', 'about', 'experience', 'projects', 'contact'].map((section) => (
               <li key={section}>
-                <Link
-                  href={`#${section}`}
-                  className="font-sans text-lg tracking-tight capitalize animate-underline-hover hover:text-accent transition-colors"
-                >
+                <Link href={`#${section}`} className="font-sans text-lg tracking-tight capitalize animate-underline-hover hover:text-accent transition-colors">
                   {section}
                 </Link>
               </li>
@@ -27,15 +23,12 @@ export function Navigation() {
           </ul>
         </div>
       </nav>
-      <nav className="fixed bottom-0 left-0 right-0 z-50 bg-background/80 backdrop-blur-md md:hidden">
+      <nav className="fixed bottom-0 left-0 right-0 z-50 bg-background/80 backdrop-blur-md md:hidden min-h-[50px]">
         <div className="px-2 py-4">
           <ul className="flex justify-around bg-background/90 p-2">
-            {["home", "about", "experience", "projects", "contact"].map((section) => (
+            {['home', 'about', 'experience', 'projects', 'contact'].map((section) => (
               <li key={section}>
-                <Link
-                  href={`#${section}`}
-                  className="font-sans text-lg tracking-tight capitalize animate-underline-hover hover:text-accent transition-colors"
-                >
+                <Link href={`#${section}`} className="font-sans text-lg tracking-tight capitalize animate-underline-hover hover:text-accent transition-colors">
                   {section}
                 </Link>
               </li>
